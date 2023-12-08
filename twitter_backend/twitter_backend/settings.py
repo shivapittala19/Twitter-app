a216adef4f76b1b7d433629e5eabddac1415246d
shivapittala19 <shivapittala2001@gmail.com>
2023-12-08 17:55:16 +0530

    production release 5

--- twitter_backend/twitter_backend/settings.py
+++ twitter_backend/twitter_backend/settings.py
@@ -133,12 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static_cdn'),
+    os.path.join(BASE_DIR, 'accounts/static'),
 ]
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
